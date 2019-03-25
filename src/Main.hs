48c386ab3d6041851693b04eb90b9ed5f23da19b
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2019-03-25 19:12:35 -0400

    latest optimised version with lower allocation rate

--- src/Main.hs
+++ src/Main.hs
@@ -2,49 +2,63 @@
 
 -- license MIT https://raw.githubusercontent.com/fromjavatohaskell/hexdump/master/LICENSE-MIT
 
-import           GHC.Word                       ( Word8(..) )
-import           Data.Maybe                     ( listToMaybe )
 import           Control.Monad                  ( when )
-import qualified System.Environment            as E
-import qualified GHC.IO.Buffer                 as Buf
+import           Data.Maybe                     ( listToMaybe )
 import           Foreign.Ptr                    ( Ptr )
 import           Foreign.Ptr                    ( plusPtr )
-import qualified System.Posix.IO               as IO
-import           System.Posix.Types             ( Fd(..) )
-import           System.Posix.Types             ( ByteCount )
+import           GHC.IO.Buffer                  ( newByteBuffer )
+import           GHC.IO.Buffer                  ( withBuffer )
+import           GHC.IO.Buffer                  ( BufferState(WriteBuffer) )
+import           GHC.Word                       ( Word8 )
+import           System.Environment             ( getArgs )
 import           System.Posix.Fcntl             ( fileAdvise )
-import           System.Posix.Fcntl             ( Advice(..) )
+import           System.Posix.Fcntl             ( Advice(AdviceSequential) )
+import           System.Posix.IO                ( fdWriteBuf )
+import           System.Posix.IO                ( fdReadBuf )
+import           System.Posix.IO                ( stdInput )
+import           System.Posix.IO                ( stdOutput )
+import           System.Posix.IO                ( openFd )
+import           System.Posix.IO                ( OpenMode(ReadOnly) )
+import           System.Posix.IO                ( defaultFileFlags )
+import           System.Posix.Types             ( Fd )
+import           System.Posix.Types             ( ByteCount )
 
 chunkSize :: ByteCount
 chunkSize = 128 * 1024
 
 -- there could be partial write
 -- so we invoke write for the rest of the buffer
--- due to some reason it happens when haskell program compiled without thread support
+-- due to some reason partial write happens when haskell program compiled without thread support
+-- and output is pipe
 writeWholeBuffer :: Fd -> Ptr Word8 -> ByteCount -> IO ()
-writeWholeBuffer fd buffer bufferLength = do
-  writeLength <- IO.fdWriteBuf fd buffer bufferLength
-  when (writeLength == 0) $ error "unable to write to output"
-  when (writeLength < bufferLength) $ do
-    let buffer' = plusPtr buffer $ fromIntegral writeLength
-    let bufferLength' = bufferLength - writeLength
-    writeWholeBuffer fd buffer' bufferLength'
+writeWholeBuffer fd buffer bufferLength = writeWholeBuffer' 0
+ where
+  writeWholeBuffer' offset = do
+    bufferLength' <- return $! bufferLength - offset
+    buffer'       <- return $! plusPtr buffer $ fromIntegral offset
+    writeLength   <- fdWriteBuf fd buffer' bufferLength'
+    when (writeLength == 0) $ error "unable to write to output"
+    when (writeLength < bufferLength') $ do
+      offset' <- return $! offset + writeLength
+      writeWholeBuffer' offset'
 
 encodeStream :: Fd -> Fd -> Ptr Word8 -> IO ()
-encodeStream fdIn fdOut buffer = do
-  readLength <- IO.fdReadBuf fdIn buffer chunkSize
-  when (readLength > 0) $ do
-    writeWholeBuffer fdOut buffer readLength
-    encodeStream fdIn fdOut buffer
+encodeStream fdIn fdOut buffer = encodeStream'
+ where
+  encodeStream' = do
+    readLength <- fdReadBuf fdIn buffer chunkSize
+    when (readLength > 0) $ do
+      writeWholeBuffer fdOut buffer readLength
+      encodeStream'
 
 main :: IO ()
 main = do
-  fdIn <- fmap listToMaybe E.getArgs >>= getHandle
+  fdIn <- fmap listToMaybe getArgs >>= getHandle
   fileAdvise fdIn 0 0 AdviceSequential
-  buffer <- Buf.newByteBuffer (fromIntegral chunkSize) Buf.WriteBuffer
-  Buf.withBuffer buffer $ encodeStream fdIn IO.stdOutput
+  buffer <- newByteBuffer (fromIntegral chunkSize) WriteBuffer
+  withBuffer buffer $ encodeStream fdIn stdOutput
  where
   getHandle (Just filename) =
-    IO.openFd filename IO.ReadOnly Nothing IO.defaultFileFlags
-  getHandle Nothing = return IO.stdInput
+    openFd filename ReadOnly Nothing defaultFileFlags
+  getHandle Nothing = return stdInput
 
