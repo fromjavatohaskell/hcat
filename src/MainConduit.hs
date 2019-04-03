306b1e85ce6c5821824cc87576c98f28bbf594eb
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2019-04-03 13:49:41 -0400

    use sourceHandleRangeWithBuffer to source input stream

--- src/MainConduit.hs
+++ src/MainConduit.hs
@@ -2,8 +2,8 @@
 
 -- license MIT https://raw.githubusercontent.com/fromjavatohaskell/hexdump/master/LICENSE-MIT
 
-import           Conduit hiding (sourceHandle)
-import           ConduitUtil (sourceHandle)
+import           Conduit
+import           Data.Conduit.Binary            ( sourceHandleRangeWithBuffer )
 import qualified System.IO as IO
 import           Data.Maybe                     ( listToMaybe )
 import           System.Environment             ( getArgs )
@@ -11,7 +11,7 @@ import           System.Environment             ( getArgs )
 main :: IO ()
 main = do
   h <- fmap listToMaybe getArgs >>= getHandle
-  runConduitRes $ sourceHandle h .| sinkHandle IO.stdout
+  runConduitRes $ sourceHandleRangeWithBuffer h Nothing Nothing 131072 .| sinkHandle IO.stdout
  where
   getHandle (Just filename) = IO.openFile filename IO.ReadMode
   getHandle Nothing = return IO.stdin
